00790bd1cf2ff5408444d177de77f39e7464976d
Daniel Cha <cha.danieldev@gmail.com>
2023-03-22 02:08:19 -0400

    keys were a string

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-    stripePublishableKey: 'process.env.STRIPE_PUBLIC_KEY',
-    stripeSecretKey: 'process.env.STRIPE_SECRET_KEY'
+    stripePublishableKey: process.env.STRIPE_PUBLIC_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY
 }
